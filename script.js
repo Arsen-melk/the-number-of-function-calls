2700f2fa1fb709a264163dd843636bed761fbed2
arsen-melk <arsenmelk001@gmail.com>
2022-11-23 16:20:25 +0400

    ֆունկցիայի կանչերի քանակը

---
+++ script.js
@@ -0,0 +1,15 @@
+let count=0;
+
+function sum(){
+    count=count+1;
+   
+
+}
+sum();
+console.log(`տպում է ${count}`);
+sum();
+console.log(`տպում է ${count}`);
+sum();
+console.log(`տպում է ${count}`);
+sum();
+console.log(`տպում է ${count}`);
